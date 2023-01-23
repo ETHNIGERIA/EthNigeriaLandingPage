8bacf78cf0591c96ac359d08629b44cb7460b6dc
Otaiki1 <otaikisadiq2804@gmail.com>
2023-01-23 05:52:47 +0100

    refac: fix navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -7,7 +7,8 @@ import { useState, useEffect } from "react";
   
 const Navbar = () => {
 
-  const[navbar, setNavbar] = useState("false")
+  const[navbar, setNavbar] = useState(false);
+  const[showNavItems, setShowNavItems] = useState(false)
 
   const changeBackground = () => {
     console.log(window.location.href.slice(-7))
@@ -25,35 +26,76 @@ const Navbar = () => {
     window.addEventListener("scroll", changeBackground)
   })
   return (
-    <div className={navbar ? `fixed flex justify-between w-[100%] z-10 text-[#fff] navbar`: `fixed flex justify-between w-full z-10`}>
-      <div className="flex md:justify-center items-center md:pl-[50px] hover:text-[#ff0000]">
-        <Link to="home">
-          <img
-            src={images.ethLogoGreen}
-            style={{ width: "24px", height: "24px" }}
-            alt="logo"
-          />
-        </Link>
+    // <div className={navbar ? `fixed flex justify-between w-[100%] z-10 text-[#fff] navbar`: `fixed flex justify-between w-full z-10`}>
+    //   <div className="flex md:justify-center items-center hover:text-[#ff0000] px-8 pt-8 md:pt-0">
+    //     <Link to="home">
+    //       <img
+    //         src={images.ethLogoGreen}
+    //         style={{ width: "24px", height: "24px" }}
+    //         alt="logo"
+    //       />
+    //     </Link>
         
-        <div className="font-extrabold text-lg text-primary">
-          <Link to="home">
-            ETHNigeria
+    //     <div className="font-extrabold text-lg text-primary">
+    //       <Link to="home">
+    //         ETHNigeria
+    //       </Link>
+    //     </div>
+        
+    //   </div>
+    //   <div>
+    //     <div className="md:hidden block">
+    //       <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-8 h-8 mt-6 mr-6">
+    //         <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+    //       </svg>
+    //     </div>
+    //     <div className="hidden md:flex flex-col md:flex-row justify-end md:justify-around items-center">
+            // <div className="font-extrabold text-lg p-6">
+            //   <Link to="/">About</Link>
+            // </div>
+            // <div className="font-extrabold text-lg p-6">
+            //   <a href="/">Events</a>  
+            // </div>
+            // <div className="font-extrabold text-lg p-6">
+            //   <Link to="contact">Contact Us</Link>
+            // </div>
+    //     </div>
+    //     </div>
+    //   </div>
+      <div className={navbar ? `fixed flex justify-between w-full z-10 text-[#fff] navbar`: `fixed flex justify-between w-full z-10 `}>
+        <div className="p-6">
+          <Link to="home" className="flex">
+            <img
+              src={images.ethLogoGreen}
+              style={{ width: "24px", height: "24px" }}
+            alt="logo"
+            />
+            <p className="font-extrabold text-lg text-primary"> ETHNigeria</p>
           </Link>
         </div>
-        
+        <div className="md:hidden block p-6" onClick={() => {setShowNavItems(!showNavItems)}}>
+          {!showNavItems ? 
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-8 h-8">
+              <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+            </svg> 
+            :
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-8 h-8">
+              <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
+            </svg>
+          }
+        </div>
+        <nav className={showNavItems ? "md:flex" :"hidden md:flex"}>
+          <div className="navbar-link">
+            <Link to="/">About</Link>
+          </div>
+          <div className="navbar-link">
+            <a href="/">Events</a>  
+          </div>
+          <div className="navbar-link">
+            <Link to="contact">Contact Us</Link>
+          </div>
+        </nav>
       </div>
-      <div className="flex justify-around items-center md:pr-[90px]">
-        <div className="font-extrabold text-lg p-6 hed">
-         <Link to="/">About</Link>
-        </div>
-        <div className="font-extrabold text-lg p-6 hed">
-          <a href="/">Events</a>
-        </div>
-        <div className="font-extrabold text-lg p-6">
-          <Link to="contact">Contact Us</Link>
-        </div>
-      </div>
-    </div>
   );
 };
 
